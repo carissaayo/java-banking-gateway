1d3aac65632f696de01312453840da96f367e7aa
Ajao Yussuf <ajaoyussufayo@gmail.com>
2026-09-22 13:44:05 +0100

    header names

--- src/main/java/com/yussufajao/gateway/routing/GatewayHeaders.java
+++ src/main/java/com/yussufajao/gateway/routing/GatewayHeaders.java
@@ -3,6 +3,9 @@ package com.yussufajao.gateway.routing;
 public final class GatewayHeaders {
 
 	public static final String ROUTE_ID = "X-Gateway-Route";
+	public static final String CORRELATION_ID = "X-Correlation-ID";
+	public static final String TRACEPARENT = "traceparent";
+	public static final String TRACESTATE = "tracestate";
 
 	private GatewayHeaders() {
 	}
