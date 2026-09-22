338d8d5c66aeb5f3073478bef76e8e4d30bfa4b4
Ajao Yussuf <ajaoyussufayo@gmail.com>
2026-09-22 13:44:25 +0100

    gateway attributes names

---
+++ src/main/java/com/yussufajao/gateway/correlation/GatewayAttributes.java
@@ -0,0 +1,11 @@
+package com.yussufajao.gateway.correlation;
+
+public final class GatewayAttributes {
+
+	public static final String CORRELATION_ID = "gateway.correlationId";
+	public static final String TRACE_ID = "gateway.traceId";
+	public static final String UPSTREAM_OUTCOME = "gateway.upstreamOutcome";
+
+	private GatewayAttributes() {
+	}
+}
